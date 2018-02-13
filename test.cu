8e754f23f31a1974f609fc899923414a3083fcb3
Samtruong <mstruong@ucdavis.edu>
2018-02-12 16:28:12 -0800

    Queue structure

--- test.cu
+++ test.cu
@@ -7,74 +7,34 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
 
 
-__global__ 
-void bin(bool * graph, int V, int* color)
+__global__
+void makeBins(int * coloredGraph, int numV, int numC, std::queue<int>* bins)
 {
-
-
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = gridDim.x * blockDim.x;
+  for(int i = index ; i < numV; i+= stride)
+  {
+    bins[coloredGraph[i]].push(i);
+  }
 }
 
-void ReadColFile(const char filename[], bool** graph, int* V)
+int main(int argc, char const *argv[])
 {
-   string line;
-   ifstream infile(filename);
-   if (infile.fail()) {
-      printf("Failed to open %s\n", filename);
-      return;
-   }
+  const int numV = 10;
+  const int numC = 5;
+  int coloredGraph[numV];
 
-   int num_rows, num_edges;
-
-   while (getline(infile, line)) {
-      istringstream iss(line);
-      string s;
-      int node1, node2;
-      iss >> s;
-      if (s == "p") {
-         iss >> s; // read string "edge"
-         iss >> num_rows;
-         iss >> num_edges;
-         *V = num_rows;
-         *graph = new bool[num_rows * num_rows];
-         memset(*graph, 0, num_rows * num_rows * sizeof(bool));
-         continue;
-      } else if (s != "e")
-         continue;
-
-      iss >> node1 >> node2;
-
-      // Assume node numbering starts at 1
-      (*graph)[(node1 - 1) * num_rows + (node2 - 1)] = true;
-      (*graph)[(node2 - 1) * num_rows + (node1 - 1)] = true;
-   }
-   infile.close();
-}
+  for(int i = 0; i <  numV; i++)
+  {
+    coloredGraph[i] = rand() % 1000 + 1;
+  }
 
 
-int main(int argc, char* argv[])
-{
-   bool* graph;
-   int V;
-   int* color;
-
-   if (string(argv[1]).find(".col") != string::npos)
-      ReadColFile(argv[1], &graph, &V);
-   else if (string(argv[1]).find(".mm") != string::npos)
-      ReadMMFile(argv[1], &graph, &V);
-   else
-      return -1;
-
- //  GraphColoring(graph, V, &color);
- //  printf("Brute-foce coloring found solution with %d colors\n", CountColors(V, color));
- //  printf("Valid coloring: %d\n", IsValidColoring(graph, V, color));
-
-   GreedyColoring(graph, V, &color);
-   printf("Greedy coloring found solution with %d colors\n", CountColors(V, color));
-   printf("Valid coloring: %d\n", IsValidColoring(graph, V, color));
-
-	
-   return 0;
+  std::queue<int> * bins;
+  cudaMallocManaged(& bins, numC*numV*sizeof(int));
+  
+  makeBins<<<1,1>>>(coloredGraph, numV, numC, bins);
+  return 0;
 }
