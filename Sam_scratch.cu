90d11b7f0e33313284edac01bafb13ba19a22e43
Samtruong <mstruong@ucdavis.edu>
2018-02-17 19:56:51 -0800

    scratch update

--- Sam_scratch.cu
+++ Sam_scratch.cu
@@ -10,6 +10,6 @@
 
 int int main(int argc, char const *argv[]) {
   int array[6] = {0,1,0,0,1,1,1,0,0,1};
-  thrust::exclusive_scan(array,array + 10,array)
+  thrust::exclusive_scan(array,array + 10,array);
   return 0;
 }
