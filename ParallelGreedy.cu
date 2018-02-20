270030a1496d0058205b601942062507e27545a3
Samtruong <mstruong@ucdavis.edu>
2018-02-19 18:59:14 -0800

    Sequence Permutation working

--- ParallelGreedy.cu
+++ ParallelGreedy.cu
@@ -7,9 +7,9 @@
 #include <iostream>
 #include <cstring>
 #include <thrust/scan.h>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
+#include <curand.h>
+#include <curand_kernel.h>
 
 using namespace std;
 
@@ -29,7 +29,60 @@ __global__ void ParallelThrust(int* h_graph, int* dimension, int V)
   for(int i = index; index < V*V; index += stride)
   {
     thrust::exclusive_scan(thrust::device,&h_graph[i*V],&h_graph[i*V+V],&h_graph[i*V]);
+    dimension[i] =  h_graph[V*i + V -1]+1;
   }
+}
+
+__global__ void PermutationGenerator(int V, int*result, int numVersion, int shuffle_degree)
+{
+  unsigned long long seed = blockDim.x;
+  unsigned long long sequence = threadIdx.x;
+  unsigned long long offset = 0;
+  curandState_t state;
+  curand_init(seed,sequence,offset,&state);
+  int index = threadIdx.x + blockDim.x * blockIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  int num1,num2,holder;
+  for(int i = index; i < V*numVersion; i+= stride){result[i] = i%V;}
+  __syncthreads();
+  for(int j = index; j<numVersion; j+=stride)
+  {
+    for(int k = 0; k < shuffle_degree; k++)
+    {
+      num1 = j*V + curand(&state) % V;
+      num2 = j*V + curand(&state) % V;
+      holder = result[num1];
+      result[num1] = result[num2];
+      result[num2] = holder;
+    }
+  }
+}
+__global__ void RandomizedParallelGreedy(int* h_graph, int* dimension,
+                       int* address, int* sequence,int V, int* result)
+{
+  int index = threadIdx.x + blockDim.x * blockIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  extern int sequence[];
+
+  __shared__ int* d_graph;
+  __shared__ int* d_dimension;
+  __shared__ int* d_address;
+
+  int length = dimension[V - 1] + address[V - 1]; //length of h_graph;
+
+  //copy to shared memory:
+  for(int i = index; i < length; i+= stride){d_graph[i] = h_graph[i];}
+  __syncthreads();
+
+  for(int i = index; i < V; i+= stride)
+  {
+    d_dimension[i] = dimension[i];
+    d_address[i] = address[i];
+  }
+  __syncthreads();
+  //end copy to shared memory
+
+  for(int j = index, j < )
 }
 //================================Utility Functions=======================================
 
@@ -99,12 +152,13 @@ void PrintMatrix(int* matrix, int M, int N) {
 }
 
 
-//===================================Main================================================
+//===================================Main=======================================
 
 int main(int argc, char* argv[])
 {
-   int* h_graph;
-   int V;
+   int* h_graph,sequence,dimension;
+   int V,numVersion;
+   numversion = 100;
    //int* color;
    if (string(argv[1]).find(".col") != string::npos)
    {
@@ -117,14 +171,21 @@ int main(int argc, char* argv[])
    else
       return -1;
 
+   cudaMallocManaged(&sequence,sizeof(int)*V*numVersion);
+   cudaMallocManaged(&dimension,sizeof(int)*V);
 
-   int dimension[V];
    ParallelThrust<<<V,V>>>(h_graph,dimension,V);
    cudaDeviceSynchronize();
-   cout<<"Scan Graph"<<endl;
-   PrintMatrix(h_graph,V,V);
+   PermutationGenerator<<<256,1024>>>(V,sequence,numVersion,V)
+   cudaDeviceSynchronize();
+   RandomizedParallelGreedy<<<>>>();
+   cudaDeviceSynchronize();
+   // cout<<"Scan Graph"<<endl;
+   // PrintMatrix(h_graph,V,V);
    // cout<<"dimension"<<endl;
    // PrintMatrix(dimension,1,V);
    cudaFree(h_graph);
+   cudaFree(dimension);
+   cudaFree(sequence);
    return 0;
 }
--- Sam_scratch.cu
+++ Sam_scratch.cu
@@ -1,18 +1,42 @@
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <set>
-#include <sstream>
-#include <string>
-#include <fstream>
 #include <iostream>
-#include <cstring>
-#include <thrust/scan.h>
+/* we need these includes for CUDA's random number stuff */
+#include <curand.h>
+#include <curand_kernel.h>
 using namespace std;
-int main(int argc, char const *argv[])
-{
-  // int array[10] = {0,1,0,0,1,1,1,0,0,1};
-  // thrust::exclusive_scan(thrust::device,array,array + 10,array);
-  for(int i = 0; i<10; i++){cout<<thrust::random<<endl;}
 
+__global__ void PermutationGenerator(int V, int*result, int numVersion, int shuffle_degree)
+{
+  unsigned long long seed = blockDim.x;
+  unsigned long long sequence = threadIdx.x;
+  unsigned long long offset = 0;
+  int index = threadIdx.x + blockDim.x * blockIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  int num1,num2,holder;
+  for(int i = index; i < V*numVersion; i+= stride){result[i] = i%V;}
+  __syncthreads();
+  curandState_t state;
+  curand_init(seed,sequence,offset,&state);
+  for(int j = index; j<numVersion; j+=stride)
+  {
+    for(int k = 0; k < shuffle_degree; k++)
+    {
+      num1 = j*V + curand(&state) % V;
+      num2 = j*V + curand(&state) % V;
+      holder = result[num1];
+      result[num1] = result[num2];
+      result[num2] = holder;
+    }
+  }
+}
+int main( )
+{
+  int* result;
+  cudaMallocManaged(&result,sizeof(int)*100);
+  PermutationGenerator<<<2,5>>>(5,result,20,5);
+  cudaDeviceSynchronize();
+  for(int i = 0; i < 100; i++){cout<<result[i];if(i % 5 == 4){cout<<endl;}  }
+  cudaFree(result);
   return 0;
 }
