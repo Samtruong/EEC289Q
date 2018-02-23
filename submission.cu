7bd34b83033872b4ef0fff9f6cda3c4cd2fa6fa0
Samtruong <mstruong@ucdavis.edu>
2018-02-22 22:20:13 -0800

    submission2

--- submission.cu
+++ submission.cu
@@ -1,6 +1,10 @@
 #include "ParallelGreedy.cu"
+#include <iostream>
+using namespace std;
 int main(int argc, char const *argv[]) {
   int** color = (int**) malloc(1*sizeof(int*));
   GraphColoringGPU(argv[1],color);
+  for(int i=0;i<16;i++)
+  {cout<<color[i]<<endl}
   return 0;
 }
